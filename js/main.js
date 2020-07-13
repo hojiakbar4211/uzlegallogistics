8ce21751e3c66f92577d511194a23d901d98107b
hojiakbar4211 <33553426+hojiakbar4211@users.noreply.github.com>
2020-07-13 10:36:33 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -796,3 +796,47 @@ $(window).load(function () {
         $('.wccm-compare-table').setDraggable();
     });
 })(jQuery);
+
+var totop = document.getElementById("gotobtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+        totop.style.display = "block";
+    } else {
+        totop.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+    $("#btnExport").click(function () {
+        $("#tblCustomers").table2excel({
+                exclude: ".noExl",
+                name:"My bio",
+                filename: "My Orders"
+        });
+    });
+
+$("body").on("click", "#btnExport1", function () {
+    html2canvas($('#tblCustomers1')[0], {
+        onrendered: function (canvas) {
+            var data = canvas.toDataURL();
+            var docDefinition = {
+                content: [{
+                    image: data,
+                    width: 500
+                }]
+            };
+            pdfMake.createPdf(docDefinition).download("MyInfo.pdf");
+        }
+    });
+});
+
+
